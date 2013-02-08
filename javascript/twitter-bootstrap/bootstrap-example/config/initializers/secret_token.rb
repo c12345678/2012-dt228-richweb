4c4fb8963b592389570e8350242d14c68ee634a1
Brian Gillespie <brian.x.gillespie@dit.ie>
2013-02-08 15:25:47 +0000

    Initial commit

---
+++ javascript/twitter-bootstrap/bootstrap-example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapExample::Application.config.secret_token = '93d5473ad95622386e5e26f0c714207486a7ccd490b9bb2682c31e3b0bbd7eb6f8ef9d843595e445db93eed8ad7fc21185b780c8fdea1602e486249960477f08'
